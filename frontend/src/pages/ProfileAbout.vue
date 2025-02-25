7fef82e448b007a5d8ce1318634691da278a4bb2
Alexandrina-Kuzeleva <alaz2005tv@gmail.com>
2025-02-25 19:43:22 +0300

    upd profile

--- frontend/src/pages/ProfileAbout.vue
+++ frontend/src/pages/ProfileAbout.vue
@@ -8,6 +8,17 @@
 			v-html="profile.data.bio"
 			class="ProseMirror prose prose-table:table-fixed prose-td:p-2 prose-th:p-2 prose-td:border prose-th:border prose-td:border-gray-300 prose-th:border-gray-300 prose-td:relative prose-th:relative prose-th:bg-gray-100 prose-sm max-w-none !whitespace-normal"
 		></div>
+
+		<!-- Email -->
+		<div v-if="profile.data.email" class="mt-3 text-gray-700 text-sm">
+			<strong>{{ __('Email') }}:</strong> {{ profile.data.email }}
+		</div>
+
+		<!-- Phone -->
+		<div v-if="profile.data.phone" class="mt-1 text-gray-700 text-sm">
+			<strong>{{ __('Phone') }}:</strong> {{ profile.data.phone }}
+		</div>
+
 		<div v-else class="text-gray-700 text-sm italic">
 			{{ __('No introduction') }}
 		</div>
