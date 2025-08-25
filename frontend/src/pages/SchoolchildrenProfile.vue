6aaf67061348e655582c5a9fb8138c0a0db9d7d5
Alexandrina-Kuzeleva <alaz2005tv@gmail.com>
2025-08-25 15:25:13 +0300

    Update SchoolchildrenProfile.vue

--- frontend/src/pages/SchoolchildrenProfile.vue
+++ frontend/src/pages/SchoolchildrenProfile.vue
@@ -409,7 +409,7 @@ async function saveProfile() {
       }).submit();
     }
     console.log('[DEBUG] Schoolprofile:', { schoolProfile});
-    let docname = schoolProfile.value?.name;
+    let docname = schoolProfile.data?.name;
     let payload = {
       doctype: 'Schoolchildren Profile',
       user: profile.data?.name,
